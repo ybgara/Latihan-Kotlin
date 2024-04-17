3a6b77413e4d70a966ab520492b42f0cd51372a8
Tegar Wibisana <95567621+ybgara@users.noreply.github.com>
2024-04-17 23:56:09 +0700

    Add files via upload

---
+++ _10-Operasi Matematika/src/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+    println("Operasi Matematika")
+
+    val result = 10.0 / 3.0
+    println(result)
+
+    val result2 = 10.0 / 3.0
+    println(result2)
+
+    var result3 = 5
+    result3 += 5
+    println(result3)
+
+    result3++
+    result3++
+    result3++
+    result3--
+    result3--
+    result3--
+
+    println(result3)
+
+    var result4 = false
+    println(!result4)
+}
