94227764214b68ebb5feaf9338bbf64127175dca
Tegar Wibisana <95567621+ybgara@users.noreply.github.com>
2024-04-17 22:25:26 +0700

    Add files via upload

---
+++ _04-tipeDataCharacter/src/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Tipe Data Character")
+
+    var Tegar: Char = 'T'
+    println(Tegar)
+
+    var Tegar2: Char = 'E'
+    println(Tegar2)
+
+    var Tegar3: Char = 'G'
+    println(Tegar3)
+
+    var Tegar4: Char = 'A'
+    println(Tegar4)
+
+    var Tegar5: Char = 'R'
+    println(Tegar5)
+}
